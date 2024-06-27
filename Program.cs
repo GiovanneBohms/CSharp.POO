b62c31ea9af09ad14c5c58d4801122ccb6824fea
Giovanne <giovannebohms@gmail.com>
2024-06-27 18:12:07 -0300

    Class

--- Program.cs
+++ Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Musica musica1 = new Musica();
+
+musica1.nome = "jao";
+musica1.artista = "Peter";
+musica1.duracao = 273;
+musica1.disponivel = true;
+
+Musica musica2 = new Musica();
+musica2.nome = "angrylaine";
+musica2.artista = "elaine";
+musica2.duracao = 280;
+musica2.disponivel = false;
+
+
+musica1.ExibirFichaTecnica();
+musica2.ExibirFichaTecnica();
