d3309c4a10aa952bf34433f7b8468bbfed55e2af
GiovanneBohms <giovannebohms@gmail.com>
2024-09-27 16:09:34 -0300

    renderização corrigida

--- BancoCTRLs/BancoCTRLs/BancoCTRLs/Program.cs
+++ BancoCTRLs/BancoCTRLs/BancoCTRLs/Program.cs
@@ -165,23 +165,25 @@ string[,] GeraPlataformaDeAray()
 
 void renderizaPlataforma(string[,] plataforma, int x, int y)
 {
- 
+    Console.Clear();
+
     plataforma[y-1,x] = " ";
     plataforma[y+1,x] = " ";
     plataforma[y,x-1] = " ";
     plataforma[y,x+1] = " ";
     plataforma[y, x] = "O";
-    
+
+    string plataformaString= "";
     
     for (int i = 0; i < tamanhoDaPlataforma; i++)
     {
         for (int j = 0; j < tamanhoDaPlataforma; j++)
         {
-            Console.Write(plataforma[i, j]);
+            plataformaString += plataforma[i, j];
         }
-        Console.WriteLine("");
+        plataformaString += "\n";
     }
-
+    Console.WriteLine(plataformaString);
     Console.WriteLine($"Coordenadas: x:{x} y:{y}");
 
 }
@@ -194,41 +196,35 @@ void MovimentaLetra() {
    
     int y = 10;
     int x = 10;
-  
-    
+
+    bool emMovimento = false;
+
     while (true)
     {
         var tecla = Console.ReadKey(true);
 
         if(tecla.Key == ConsoleKey.W && y>1)
         {
-         
             y--;
-            Console.Clear();
             renderizaPlataforma(plataforma,x,y);
         }
         if (tecla.Key == ConsoleKey.S && y < tamanhoDaPlataforma-2)
         {
-        
             y++;
-            Console.Clear();
             renderizaPlataforma(plataforma, x, y);
         }
 
         if (tecla.Key == ConsoleKey.A && x>1)
         {
-          
             x--;
-            Console.Clear();
             renderizaPlataforma(plataforma, x, y);
         }
         if (tecla.Key == ConsoleKey.D && x< tamanhoDaPlataforma-2)
         {
-           
             x++;
-            Console.Clear();
             renderizaPlataforma(plataforma, x, y);
         }
+        Thread.Sleep(50);
     }
 }
 
